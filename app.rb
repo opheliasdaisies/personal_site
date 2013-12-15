23946d147bdee6ebfc2204bac74291d5a7442661
Sara Gorecki <sgorecki@gmail.com>
2013-12-15 17:11:46 -0500

    add initial files for sinatra app

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'bundler'
+Bundler.require
+
+module Site
+  class PersonalSite < Sinatra::Application
+
+    get '/' do
+      
+    end
+
+  end
+end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './app'
+
+run Site::PersonalSite.new
